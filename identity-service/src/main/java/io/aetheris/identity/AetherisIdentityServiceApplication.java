71554a03a5b280800c19e2a5ac63cf0485e7277a
Poojana Kaveesh Sellahewa <teldigi5@gmail.com>
2026-09-10 09:57:18 +0530

    Add identity service application

---
+++ identity-service/src/main/java/io/aetheris/identity/AetherisIdentityServiceApplication.java
@@ -0,0 +1,11 @@
+package io.aetheris.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherisIdentityServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AetherisIdentityServiceApplication.class, args);
+    }
+}
