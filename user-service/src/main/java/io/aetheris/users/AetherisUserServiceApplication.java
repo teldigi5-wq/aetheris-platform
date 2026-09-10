85395b5f56eb477974ada2c5db4c579ad44dea38
Poojana Kaveesh Sellahewa <teldigi5@gmail.com>
2026-09-10 10:49:15 +0530

    Stage 3: enable user-service caching

--- user-service/src/main/java/io/aetheris/users/AetherisUserServiceApplication.java
+++ user-service/src/main/java/io/aetheris/users/AetherisUserServiceApplication.java
@@ -2,7 +2,9 @@ package io.aetheris.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class AetherisUserServiceApplication {
     public static void main(String[] args) {
