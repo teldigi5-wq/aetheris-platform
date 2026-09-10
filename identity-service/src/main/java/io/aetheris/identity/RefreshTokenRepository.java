dd0a50d83e55d61e3e5c83f19048b97b01295a4d
Poojana Kaveesh Sellahewa <teldigi5@gmail.com>
2026-09-10 10:26:43 +0530

    Stage 2.4: add refresh token repository

---
+++ identity-service/src/main/java/io/aetheris/identity/RefreshTokenRepository.java
@@ -0,0 +1,8 @@
+package io.aetheris.identity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+}
