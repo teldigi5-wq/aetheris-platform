58ce1c71186784d20e2cfe1b196ecc2d5e4b4018
Poojana Kaveesh Sellahewa <teldigi5@gmail.com>
2026-09-10 11:50:03 +0530

    Stage 4: add audit service application

---
+++ audit-service/src/main/java/io/aetheris/audit/AetherisAuditServiceApplication.java
@@ -0,0 +1,11 @@
+package io.aetheris.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherisAuditServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AetherisAuditServiceApplication.class, args);
+    }
+}
