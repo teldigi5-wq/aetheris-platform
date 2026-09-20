eef610a0797cf2f6465ccfde533df0a7b68101dd
Poojana Kaveesh Sellahewa <teldigi5@gmail.com>
2026-09-20 11:16:02 +0530

    fix: disambiguate provider identity resolver injection

--- orchestrator-service/src/main/java/io/aetheris/orchestrator/connector/oauth/ProviderIdentityResolver.java
+++ orchestrator-service/src/main/java/io/aetheris/orchestrator/connector/oauth/ProviderIdentityResolver.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.aetheris.orchestrator.connector.ConnectorProvider;
 import io.aetheris.orchestrator.connector.action.ConnectorActionBlockedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -23,6 +24,7 @@ public class ProviderIdentityResolver {
     private final ObjectMapper mapper;
     private final HttpClient http;
 
+    @Autowired
     public ProviderIdentityResolver(OAuthProviderRegistry providers, ObjectMapper mapper) {
         this(providers, mapper, HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(8)).build());
     }
